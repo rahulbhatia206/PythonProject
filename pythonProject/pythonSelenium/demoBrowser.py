71aa1c276ac55da60eb495240ad282a79cf26a64
rahulbhatia206 <rahulbhatia206@gmail.com>
2022-06-12 04:42:53 +0530

    complete browser code

--- pythonProject/pythonSelenium/demoBrowser.py
+++ pythonProject/pythonSelenium/demoBrowser.py
@@ -2,10 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+import time
 
 options = Options()
-options.add_experimental_option("detach", True)
+# options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://google.com")
+driver.get("https://rahulshettyacademy.com")
+
+print(driver.title)
+print(driver.current_url)
+
+time.sleep(10)
+driver.close()
